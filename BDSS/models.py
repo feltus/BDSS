9c9ba7c2cad062906eb11ed6fbf09f2898633018
Nick Watts <nick@nawatts.com>
2014-12-04 16:10:44 -0500

    Allow users to generate SSH keys for destination machines.

--- BDSS/models.py
+++ BDSS/models.py
@@ -44,6 +44,44 @@ class User(BaseModel, UserMixin):
     #  Jobs this user has submitted.
     jobs = relationship('Job', backref='owner')
 
+    ## @var keys
+    #  SSH keys owned by this user.
+    keys = relationship('SSHKey', backref='owner')
+
+class SSHKey(BaseModel):
+
+    __tablename__ = 'ssh_keys'
+
+    __table_args__ = (
+        # Prevent duplicate keys for a user/host pair.
+        UniqueConstraint('owner_id', 'destination', name='_duplicate_keys_constraint'),
+    )
+
+    ## @var id
+    #  Unique ID for this key.
+    id = Column(Integer(), primary_key=True, nullable=False)
+
+    ## @var owner_id
+    #  The ID of the user who owns this key.
+    owner_id = Column(Integer(), ForeignKey('users.id'), nullable=False)
+
+    ## @var destination
+    #  The data destination this key is for. This corresponds to a key
+    #  in config/data_destinations.yml.
+    destination = Column(String(100), nullable=False)
+
+    ## @var username
+    #  The user's username on the destination host.
+    username = Column(String(50), nullable=False)
+
+    ## @var public
+    #  The public key.
+    public = Column(Text(), nullable=False)
+
+    ## @var private
+    #  The private key.
+    private = Column(Text(), nullable=False)
+
 
 class Job(BaseModel):
 
