47b9ecc3b3f62c636330bbbc1812fa3416164b2b
Nick Watts <nick@nawatts.com>
2014-12-30 10:24:46 -0500

    Convert SSH key generation to new form submission/error methods. Add validation for key generation form.

--- BDSS/routes/keys.py
+++ BDSS/routes/keys.py
@@ -5,6 +5,7 @@ from StringIO import StringIO
 
 from ..common import config
 from ..models import SSHKey
+from .util import filter_params, json_response
 
 key_routes = Blueprint('keys', __name__)
 
@@ -18,26 +19,34 @@ def list_keys():
 @key_routes.route('', methods=['POST'])
 @login_required
 def create_key():
+    params = filter_params(request.get_json(), ['username', 'destination'])
 
-    username = request.form.get('username')
-    destination = request.form.get('destination')
+    errors = {}
+    if 'username' not in params.keys() or len(params['username']) == 0:
+        errors['username'] = 'A username is required'
 
-    if not username or not destination:
-        return list_keys()
+    # Prevent duplicate keys for a destination.
+    key = g.db_session.query(SSHKey).filter_by(owner=current_user, destination=params['destination']).first()
+    if key is not None:
+        errors['destination'] = 'You already have a key for this destination'
+
+    if errors:
+        return json_response({'field_errors': errors}, status=400)
 
     k = RSAKey.generate(2048)
     out = StringIO()
     k.write_private_key(out)
 
-    key = SSHKey(owner=current_user, username=username, destination=destination)
-    comment = "bdss_{email}_{destination}".format(email=current_user.email, destination=destination)
+    key = SSHKey(**params)
+    key.owner = current_user
+    comment = "bdss_{email}_{destination}".format(email=current_user.email, destination=key.destination)
     key.public = "{type} {key} {comment}\n".format(type=k.get_name(), key=k.get_base64(), comment=comment)
     key.private = out.getvalue()
 
     g.db_session.add(key)
     g.db_session.commit()
 
-    return list_keys()
+    return json_response({}, status=201)
 
 @key_routes.route('/<key_id>', methods=['GET'])
 @login_required
