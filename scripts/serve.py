353b70bf17db5ed5d89029a69e2b80b4df44249e
Nick Watts <nick@nawatts.com>
2015-01-22 14:44:35 -0500

    Reload Flask server when a config file is changed.

--- scripts/serve.py
+++ scripts/serve.py
@@ -3,12 +3,21 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, project_dir)
 
 from BDSS import app as application
 
+config_dir = os.path.join(project_dir, 'BDSS', 'config')
+config_files = []
+for f in os.listdir(config_dir):
+    fpath = os.path.join(config_dir, f)
+    if os.path.isfile(fpath) and fpath.endswith('.yml'):
+        config_files.append(fpath)
+
 args = {
-    'debug': True
+    'debug': True,
+    'extra_files': config_files
 }
 
 if len(sys.argv) > 1 and sys.argv[1] == '--public':
