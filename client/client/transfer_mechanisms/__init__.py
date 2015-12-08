c2319c9a0602418559dff9d672abb91ba5093dcf
Nick Watts <nick@nawatts.com>
2015-12-08 15:10:39 -0500

    Utility functions for transfer mechanism modules.

---
+++ client/client/transfer_mechanisms/__init__.py
@@ -0,0 +1,27 @@
+import importlib
+import pkgutil
+
+
+def available_mechanisms():
+    """
+    Names of all available transfer mechanisms.
+    """
+    return [name for _, name, _ in pkgutil.iter_modules(__path__)]
+
+
+def default_mechanism(url):
+    """
+    Name of the default transfer mechanism for a URL.
+    This is used if the metadata repository doesn't specify a mechanism.
+    """
+    return "curl"
+
+
+def transfer_mechanism_module(mechanism_name):
+    """
+    The Python module for a transfer mechanism.
+    """
+    try:
+        return importlib.import_module(__package__ + "." + mechanism_name)
+    except ImportError:
+        return None
