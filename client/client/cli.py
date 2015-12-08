6bca72c78a3da84b343ad9a872fcb7a269aad593
Nick Watts <nick@nawatts.com>
2015-12-08 14:40:30 -0500

    Parse CLI args to get URLs

--- client/client/cli.py
+++ client/client/cli.py
@@ -1,2 +1,26 @@
+import argparse
+import sys
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description="BDSS client")
+
+    parser.add_argument("urls",
+                        help="URLs of data files to download",
+                        metavar="data-file-urls",
+                        nargs="*")
+
+    parser.add_argument("-m", "--url-manifest",
+                        dest="manifest_file",
+                        help="Path to file containing a list of URLs of data files to download.",
+                        type=argparse.FileType("r"))
+
+    args = parser.parse_args()
+
+    if args.manifest_file:
+        args.urls = [line.strip() for line in args.manifest_file if line.strip()]
+
+    if not args.urls:
+        parser.print_help(file=sys.stderr)
+        sys.exit(1)
+
+    print(args.urls)
