353e155fa9f2b109cf523845dc4d06219a04b609
Nick Watts <nick@nawatts.com>
2016-01-26 15:08:09 -0500

    Add web interface for admins to grant/revoke permissions to other users. Closes #77

--- metadata_repository/app/routes.py
+++ metadata_repository/app/routes.py
@@ -123,6 +123,64 @@ def admin_required(func):
 
 ######################################################################################################
 #
+# Users
+#
+######################################################################################################
+
+
+@routes.route("/users")
+@login_required
+@admin_required
+def list_users():
+    """
+    List all users in database
+    """
+    page_num = int(request.args.get("page", 1))
+    num_users_per_page = 10
+    users = User.query.order_by(User.is_admin.desc()).order_by(User.name) \
+        .limit(num_users_per_page).offset((page_num - 1) * num_users_per_page).all()
+    total_num_users = User.query.count()
+    total_num_pages = math.ceil(total_num_users / num_users_per_page)
+    page_range = range(max(1, page_num - 3), min(total_num_pages, page_num + 3) + 1)
+    return render_template("users/index.html.jinja", users=users, page_num=page_num, page_range=page_range,
+                           total_num_pages=total_num_pages)
+
+
+@routes.route("/users/<user_id>")
+@login_required
+@admin_required
+def show_user(user_id):
+    """
+    Show information about a specific user
+    """
+    user = User.query.filter(User.user_id == user_id).first()
+    return render_template("users/show.html.jinja", user=user)
+
+
+@routes.route("/users/<user_id>/edit_permissions", methods=["GET", "POST"])
+@login_required
+@admin_required
+def edit_user_permissions(user_id):
+    """
+    Grant/revoke admin permissions for a user
+    """
+    user = User.query.filter(User.user_id == user_id).first()
+    if request.method == "POST":
+        user.is_admin = not user.is_admin
+        try:
+            db_session.commit()
+            flash("Permissions updated", "success")
+            return redirect(url_for("routes.show_user", user_id=user.user_id))
+        except:
+            db_session.rollback()
+            flash("Failed to update permissions", "danger")
+            traceback.print_exc()
+
+    return render_template("users/edit_permissions.html.jinja", user=user)
+
+
+######################################################################################################
+#
 # Data sources
 #
 ######################################################################################################
