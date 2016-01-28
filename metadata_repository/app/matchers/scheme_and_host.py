5e166be080763ff9fcbf3526f8d1c972dd5ba6ef
Nick Watts <nick@nawatts.com>
2016-01-28 14:04:49 -0500

    Use string field instead of select for choosing scheme to match

--- metadata_repository/app/matchers/scheme_and_host.py
+++ metadata_repository/app/matchers/scheme_and_host.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlparse
 
 from wtforms import Form
-from wtforms.fields import SelectField, StringField
+from wtforms.fields import StringField
 from wtforms.validators import InputRequired
 
 label = "Scheme and Host"
@@ -15,17 +15,9 @@ def matches_url(options, url):
         return False
 
 
-# List of URL schemes supported by urlparse
-# https://docs.python.org/3/library/urllib.parse.html
-_supported_schemes = ["file", "ftp", "gopher", "hdl", "http", "https", "imap", "mailto", "mms", "news", "nntp",
-                      "prospero", "rsync", "rtsp", "rtspu", "sftp", "shttp", "sip", "sips", "snews", "svn", "svn+ssh",
-                      "telnet", "wais"]
-
-
 class OptionsForm(Form):
 
-    scheme = SelectField(label="Scheme",
-                         choices=[(s, s) for s in _supported_schemes],
+    scheme = StringField(label="Scheme",
                          validators=[InputRequired()])
 
     host = StringField(label="Host",
