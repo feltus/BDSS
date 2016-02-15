de673df69877cd60cd9b921c6a06a5502f1ac888
Nick Watts <nick@nawatts.com>
2016-02-15 09:48:19 -0500

    Exclude shared base module from list of available transfer mechanisms

--- client/client/transfer_mechanisms/__init__.py
+++ client/client/transfer_mechanisms/__init__.py
@@ -2,11 +2,14 @@ import importlib
 import pkgutil
 
 
+EXCLUDE_MODULES = ("base",)
+
+
 def available_mechanisms():
     """
     Names of all available transfer mechanisms.
     """
-    return [name for _, name, _ in pkgutil.iter_modules(__path__)]
+    return [name for _, name, _ in pkgutil.iter_modules(__path__) if name not in EXCLUDE_MODULES]
 
 
 def default_mechanism(url):
