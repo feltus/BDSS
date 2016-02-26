feltus/BDSS

commit a57f3176d72d832eed541277740d6d2dd387f88d

Switch to explicit imports to get PyInstaller to work.

client/client/transfer_mechanisms/__init__.py
@@ -16,19 +16,21 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-import importlib
-import pkgutil
+from . import aspera
+from . import curl
 
 
-EXCLUDE_MODULES = ("base",)
+_mechanisms = {
+    "aspera": aspera,
+    "curl": curl
+}
 
 
 def available_mechanisms():
     """
     Names of all available transfer mechanisms.
     """
-    all_mechanisms = [name for _, name, _ in pkgutil.iter_modules(__path__) if name not in EXCLUDE_MODULES]
-    return [m for m in all_mechanisms if transfer_mechanism_module(m).is_available()]
+    return list(_mechanisms.keys())
 
 
 def default_mechanism(url):
@@ -46,7 +48,4 @@ def transfer_mechanism_module(mechanism_name):
     """
     The Python module for a transfer mechanism.
     """
-    try:
-        return importlib.import_module(__package__ + "." + mechanism_name)
-    except ImportError:
-        return None
+    return _mechanisms[mechanism_name]

client/client/actions/__init__.py
@@ -16,20 +16,26 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-import importlib
-import pkgutil
-import re
+from . import mechanisms_action
+from . import sources_action
+from . import test_files_action
+from . import transfer_action
+
+
+_actions = {
+    "mechanisms": mechanisms_action,
+    "sources": sources_action,
+    "test_files": test_files_action,
+    "transfer": transfer_action
+}
 
 
 def all_actions():
-    return [re.sub(r"_action$", "", name) for _, name, _ in pkgutil.iter_modules(__path__)]
+    return list(_actions.keys())
 
 
 def action_module(action_name):
-    try:
-        return importlib.import_module(__package__ + "." + action_name + "_action")
-    except ImportError:
-        return None
+    return _actions[action_name]
 
 
 def available_action_info():

client/client.spec
@@ -19,10 +19,10 @@
 block_cipher = None
 
 
-a = Analysis(['run.py'],
-             pathex=['/vagrant'],
+a = Analysis(["run.py"],
+             pathex=["/vagrant"],
              binaries=None,
-             datas=None,
+             datas=[("client/defaults.cfg", "client")],
              hiddenimports=[],
              hookspath=[],
              runtime_hooks=[],
@@ -37,7 +37,7 @@ exe = EXE(pyz,
           a.binaries,
           a.zipfiles,
           a.datas,
-          name='bdss',
+          name="bdss",
           debug=False,
           strip=False,
           upx=True,

client/run.py
@@ -19,6 +19,6 @@
 # PyInstaller won't work directly on client/__main__.py because it needs
 # client to be loaded as a module. Thus, this wrapper script.
 
-import client
+from client.__main__ import main
 
-client.__main__.main()
+main()

commit b095826a13511aef95b63272471193bc803fe587

Return only available mechanisms. This got broken in a57f3176d72d832eed541277740d6d2dd387f88d.

client/client/transfer_mechanisms/__init__.py
@@ -30,7 +30,7 @@ def available_mechanisms():
     """
     Names of all available transfer mechanisms.
     """
-    return list(_mechanisms.keys())
+    return [m for m in list(_mechanisms.keys()) if transfer_mechanism_module(m).is_available()]
 
 
 def default_mechanism(url):