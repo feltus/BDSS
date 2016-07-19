969473c9e9a1f388315ed359d94ca0b02098564b
Nick Watts <nick@nawatts.com>
2016-07-19 10:29:37 -0400

    Use in memory SQLite database for tests

--- metadata_repository/scripts/run_tests.sh
+++ metadata_repository/scripts/run_tests.sh
@@ -4,7 +4,7 @@ SCRIPTS_DIR=$( cd $(dirname $0); pwd -P )
 PROJECT_DIR=$( cd "${SCRIPTS_DIR}/.."; pwd -P )
 
 export CSRF_DISABLED=True
-export DATABASE_URL="sqlite:///${PROJECT_DIR}/test.sqlite"
+export DATABASE_URL="sqlite://"
 
 cd "$PROJECT_DIR"
 nose2 \
