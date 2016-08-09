feltus/BDSS

commit 027bd8da57323e3b343d0eab9022d61fb598ab44

Fix bug preventing transfer reports from being uploaded.
If the destination property was set instead of destination_id,
TrackEditsMixin would try to update properties on the destination, which
would fail because the route does not require authentication.

metadata_repository/app/routes/transfer_reports.py
@@ -49,7 +49,7 @@ def report_transfer():
             report = TransferReport(
                 data_source_id=data_source.id,
                 report_id=max([r.report_id for r in data_source.transfer_reports] + [0]) + 1,
-                destination=destination,
+                destination_id=destination.id,
                 url=form.url.data,
                 file_size_bytes=form.file_size_bytes.data,
                 transfer_duration_seconds=form.transfer_duration_seconds.data,

commit 2b09910809eaaed86d95d641c45e202a277d1a6c

Fix uploading transfer reports without a destination. Broke it in 027bd8da57323e3b343d0eab9022d61fb598ab44.

metadata_repository/app/routes/transfer_reports.py
@@ -42,14 +42,15 @@ def report_transfer():
     error_message = None
     if form.validate():
         data_source = matching_data_source(form.url.data)
-        destination = None
+        destination_id = None
         if form.destination.data:
             destination = Destination.query.filter(Destination.label == form.destination.data).first()
+            destination_id = destination.id
         if data_source:
             report = TransferReport(
                 data_source_id=data_source.id,
                 report_id=max([r.report_id for r in data_source.transfer_reports] + [0]) + 1,
-                destination_id=destination.id,
+                destination_id=destination_id,
                 url=form.url.data,
                 file_size_bytes=form.file_size_bytes.data,
                 transfer_duration_seconds=form.transfer_duration_seconds.data,